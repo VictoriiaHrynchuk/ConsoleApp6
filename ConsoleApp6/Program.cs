115f1720592097e4c00f6d6fb9ce784a1f330b19
Markian <markijanchyk@gmail.com>
2021-10-16 13:49:04 +0300

    Second commit.

--- ConsoleApp6/Program.cs
+++ ConsoleApp6/Program.cs
@@ -6,7 +6,18 @@ namespace ConsoleApp6
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            for (int j = 0; j <=6; j++)
+            {
+                 for (int i = 1; i <=10; i++)
+                            {
+                                Console.Write("*");
+                            }
+
+                 Console.WriteLine();
+            }
+
+            Console.ReadLine();
+
         }
     }
 }
